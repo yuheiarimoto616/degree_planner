d694d303e3b20c956c9105d6ea31624f7bf5335b
Yuhei <yuhei61627@icloud.com>
2022-02-10 01:49:55 -0800

    Implemented basic console application (e.g. printing instructions)

--- src/main/ui/DegreePlannerApp.java
+++ src/main/ui/DegreePlannerApp.java
@@ -6,25 +6,24 @@ import model.DegreePlanner;
 import java.util.Locale;
 import java.util.Scanner;
 
+// Degree planner application
 public class DegreePlannerApp {
     private static DegreePlanner degreePlanner;
     private static Scanner scanner;
-    private static Main main;
 
     public DegreePlannerApp() {
         degreePlanner = new DegreePlanner();
         scanner = new Scanner(System.in);
-        main = new Main();
 
-        Boolean isQuit = false;
+        boolean isQuit = false;
 
         while (!isQuit) {
-            main.printInstruction();
+            printInstruction();
 
             String action = scanner.nextLine();
 
             if (action.equals("add")) {
-                main.addOperation();
+                addOperation();
             } else if (action.equals("view")) {
                 degreePlanner.printCourses();
             } else if (action.equals("grade")) {
@@ -42,19 +41,23 @@ public class DegreePlannerApp {
         System.out.println("    - view (to view your courses)");
         System.out.println("    - grade (to get average grade)");
         System.out.println("    - GPA");
-        System.out.println("    - quit");
+        System.out.println("    - q (to quit)");
     }
 
     public void addOperation() {
         System.out.println("Subject code (e.g. CPSC):");
         String subjectCode = scanner.nextLine().toUpperCase(Locale.ROOT);
+
         System.out.println("Course number (e.g. 210):");
         String courseNum = scanner.nextLine();
+
         System.out.println("Number of credits:");
         int credits = scanner.nextInt();
-        System.out.println("Status (choose 0 for passed, 1 for currently taking, and 2 for planning):");
+
+        System.out.println("Status (choose 0 for completed, 1 for currently taking, and 2 for planning):");
         int status = scanner.nextInt();
-        degreePlanner.addCourse(new Course(subjectCode, courseNum, credits, status));
+        Course course = new Course(subjectCode, courseNum, credits, status);
+        degreePlanner.addCourse(course);
         String buffer = scanner.nextLine();
     }
 }
--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -1,60 +1,7 @@
 package ui;
 
-import model.DegreePlanner;
-import model.Course;
-
-import java.util.Locale;
-import java.util.Scanner;
-
 public class Main {
-    private static DegreePlanner degreePlanner;
-    private static Scanner scanner;
-    private static Main main;
-
     public static void main(String[] args) {
-        degreePlanner = new DegreePlanner();
-        scanner = new Scanner(System.in);
-        main = new Main();
-
-        Boolean isQuit = false;
-
-        while (!isQuit) {
-            main.printInstruction();
-
-            String action = scanner.nextLine();
-
-            if (action.equals("add")) {
-                main.addOperation();
-            } else if (action.equals("view")) {
-                degreePlanner.printCourses();
-            } else if (action.equals("grade")) {
-                System.out.println(degreePlanner.calculateAvgGrade());
-            } else if (action.equals("quit")) {
-                isQuit = true;
-            }
-        }
-    }
-
-    // EFFECTS: print out the instruction
-    public void printInstruction() {
-        System.out.println("Please select an option:");
-        System.out.println("    - add (to add course)");
-        System.out.println("    - view (to view your courses)");
-        System.out.println("    - grade (to get average grade)");
-        System.out.println("    - GPA");
-        System.out.println("    - quit");
-    }
-
-    public void addOperation() {
-        System.out.println("Subject code (e.g. CPSC):");
-        String subjectCode = scanner.nextLine().toUpperCase(Locale.ROOT);
-        System.out.println("Course number (e.g. 210):");
-        String courseNum = scanner.nextLine();
-        System.out.println("Number of credits:");
-        int credits = scanner.nextInt();
-        System.out.println("Status (choose 0 for passed, 1 for currently taking, and 2 for planning):");
-        int status = scanner.nextInt();
-        degreePlanner.addCourse(new Course(subjectCode, courseNum, credits, status));
-        String buffer = scanner.nextLine();
+        new DegreePlannerApp();
     }
 }
